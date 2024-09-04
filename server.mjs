583d401fb91dd63109739808118e9a07cc09fba8
CORA6754 <104596319+CORA6754@users.noreply.github.com>
2024-09-04 06:19:04 -0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -13,3 +13,31 @@ server.listen(3000, '127.0.0.1', () => {
 });
 
 // run with `node server.mjs`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening on 127.0.0.1:3000');
+  });
+  
+// run with `node server.mjs`
